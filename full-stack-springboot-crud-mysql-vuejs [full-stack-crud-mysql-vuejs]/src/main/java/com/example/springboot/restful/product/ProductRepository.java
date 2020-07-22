8ea09b5e9ec42360ae60f623ab5f48fcf3d183f7
VuDucLong1807 <44286058+VuDucLong1807@users.noreply.github.com>
2020-07-22 17:03:48 +0700

    Add files via upload

---
+++ full-stack-springboot-crud-mysql-vuejs [full-stack-crud-mysql-vuejs]/src/main/java/com/example/springboot/restful/product/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.example.springboot.restful.product;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product,Long> {
+
+}
